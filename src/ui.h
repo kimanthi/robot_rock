c318ecad6ac1112336da6e63d286e3fc2306605e
Thiago Scalone <thiago@scalone.com.br>
2014-10-31 19:17:11 -0200

    UI defines screen x and y, line width and height definition.

--- src/ui.h
+++ src/ui.h
@@ -9,4 +9,10 @@ XuiFont *OpenFont();
 XuiFont *GetFont();
 void CloseFont();
 
+#define SCREEN_X 320
+#define SCREEN_Y 240
+
+#define LINE_WIDTH 16
+#define LINE_HEIGHT 32
+
 #endif  /* PRINT_TEXT_H */
