4bc11380d13cf07f293c0adfd2bf61a81296e34e
Thiago Scalone <thiago@scalone.com.br>
2014-11-13 17:46:37 -0200

    Remove test and useless comment in main.

--- src/main.c
+++ src/main.c
@@ -239,72 +239,10 @@ int DeInit()
     return 0;
 }
 
-void testSwipe(void)
-{
-  unsigned char buf[2250];
-  int handle;
-  ST_MSR_DATA track1;
-  ST_MSR_DATA track2;
-  ST_MSR_DATA track3;
-  int ret=0;
-
-  memset(&track1, 0, sizeof(track1));
-  memset(&track2, 0, sizeof(track2));
-  memset(&track3, 0, sizeof(track3));
-
-  /*ret = OsMsrOpen();*/
-
-  /*display("Before Open");*/
-  /*if (ret != RET_OK) {*/
-    /*display("Open Error: %d", ret);*/
-    /*return;*/
-  /*}*/
-
-  /*OsMsrReset();*/
-
-  display("Before Swipe");
-  ret = FALSE;
-  handle = open("/dev/msr", O_RDONLY);
-  while(ret != TRUE && ret != ERR_DEV_NOT_OPEN) {
-    memset(buf, 0, sizeof(buf));
-    ret = read(handle, &buf, 2250);
-    display("%d - %d - %s", handle, ret, buf);
-    sleep(1);
-    /*ret = OsMsrSwiped();*/
-  }
-  close(handle);
-  /*display("Swipe: %d", ret);*/
-
-  /*OsMsrRead(&track1, &track2, &track3);*/
-  /*display("t1 %s", track1.TrackData);*/
-  /*display("t2 %s", track2.TrackData);*/
-  /*display("t3 %s", track3.TrackData);*/
-
-  /*display("Before Close");*/
-  /*OsMsrClose();*/
-}
-
-void displayImg(void)
-{
-	XuiImg *img;
-
-	img  = XuiImgLoadFromFile("./qrcode.bmp");
-  img->width = 120;
-  img->height = 120;
-
-	XuiCanvasDrawImg(XuiRootCanvas(), 0, 0, 120, 120, XUI_BG_NORMAL, img);
-
-  sleep(4);
-
-	XuiImgFree(img);
-}
-
 int main(int argc, char **argv)
 {
     OsLog(LOG_INFO, "Teste");
     Init();
-
-    /*testSwipe();*/
 
     robot_rock_execute();
     DeInit();
