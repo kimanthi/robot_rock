233fded203e91c7ffc312ec8a59d4216f2c34f91
Thiago Scalone <thiago@scalone.com.br>
2014-11-14 19:36:25 -0200

    Add https test.

--- mrblib/init.rb
+++ mrblib/init.rb
@@ -84,6 +84,22 @@ module TestRobotRock
     getc
   end
 
+  def self.test_https
+    socket = TCPSocket.new('polarssl.org', 443)
+    entropy = PolarSSL::Entropy.new
+    ctr_drbg = PolarSSL::CtrDrbg.new(entropy)
+    ssl = PolarSSL::SSL.new
+    ssl.set_endpoint(PolarSSL::SSL::SSL_IS_CLIENT)
+    ssl.set_authmode(PolarSSL::SSL::SSL_VERIFY_NONE)
+    ssl.set_rng(ctr_drbg)
+    ssl.set_socket(socket)
+    ssl.handshake
+    ssl.write("GET / HTTP/1.0\r\nHost: polarssl.org\r\n\r\n")
+    response = ""
+    while chunk = ssl.read(1024)
+      response << chunk
+    end
+    p "https response size: #{response.size}"
     getc
   end
 
