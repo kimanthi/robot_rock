8d5f560017225b9a43ca098137804d3fb634f4f9
Thiago Scalone <thiago@scalone.com.br>
2014-12-10 23:25:19 -0200

    Main changes: - Refactor CloudWalkInit.execute to use new strategy. - Use Device::Helper to perform menu, check_error and form. - Remove class DebugInit. - Implement flow using Device::ParamsDat(parse_apps, update_app, update_apps and application_menu). - Remove class Download.

--- main.rb
+++ main.rb
@@ -6,159 +6,35 @@ class Main < Device
     Device::Display.print("CloudWalk", 1, 5)
     Device::Display.print("Serial #{Device::System.serial}", 2, 4)
 
-    Device::Display.print(" 1 - DebugInit", 5)
-    Device::Display.print(" N - CloudWalkInit", 6)
+    Device::Display.print(" 1 - CloudWalkInit", 5)
 
     if getc == "1"
-      DebugInit.perform
-    else
       CloudWalkInit.perform
     end
-  end
-
-  def self.form(txt, min=0, max=8, default="", is_number=true)
-    Device::Display.clear
-    puts txt
-
-    puts "\n"
-    if is_number
-      string = get_string(min, max, IO_INPUT_NUMBERS)
-    else
-      string = get_string(min, max)
-    end
-    return default if string.empty?
-    string
-  end
-
-  class Download
-    class << self
-      attr_accessor :apps
-      @apps  = []
-    end
-
-    def self.attach
-      Device::Display.clear
-      Device::Display.print("Download App")
-      puts "Connecting..."
-      if Device::Network.connected? < 0
-        if (ret = Device::Network.attach) == 0
-          puts "Connected #{ret}"
-        else
-          puts "Attach fail #{ret}"
-          return false
-        end
-      else
-        puts "Already connected"
-      end
-      true
-    end
-
-    def self.update_app(file)
-      # TODO Scalone: Refactor
-      if self.attach
-        puts "Donwloading #{file}..."
-        ret = Device::Transaction::Download.request_file("#{file.split(".").first}.rb", file)
-        Device::Network.walk_socket.close unless Device::Network.walk_socket.closed?
-
-        check_error ret
-      end
-    end
-
-    def self.params_dat
-      if self.attach
-        puts "Downloading Params..."
-        puts "#{ret = Device::Transaction::Download.request_param_file}"
-        Device::Network.walk_socket.close unless Device::Network.walk_socket.closed?
-
-        parse_apps if ret = check_error(ret)
-        ret
-      end
-    end
-
-    def self.check_error(ret)
-      value = true
-      case ret
-      when 3
-        puts "Serial number not found."
-        value =  false
-      when 2
-        puts "File not found."
-        value = false
-      when 1
-        puts "File is the same."
-      when 0
-        puts "Success."
-      when -1
-        puts "Communication failure."
-        value = false
-      when -2
-        puts "Encoding error."
-        value = false
-      when -3
-        puts "IO Error."
-        value = false
-      else
-        puts "Communication fail."
-        value = false
-      end
-
-      value
-    end
-
-    def self.parse_apps
-      @params = FileDb.new("./params.dat") if File.exists?("./params.dat")
-      @apps   = []
-
-      @params["apps_list"].gsub("\"", "").split(";").each do |app|
-        values = app.split(",")
-        mrb    = "#{values[1].gsub("#{Device::Setting.company_name}_", "").split(".")[0]}.mrb"
-        @apps << {:label => values[0], :mrb => mrb, :file => values[1], :available => values[2], :crc => values[3]}
-      end
-    end
-
-    def self.update_apps
-      @apps ||= []
-      Device::Display.clear
-      Device::Display.print("Press to Download #{@apps.size} apps", 3)
-      getc
-      @apps.each do |app|
-        update_app(app[:mrb])
-      end
-    end
-
-    def self.menu
-      Device::Display.clear
-      Device::Display.print("Application Menu")
-      selected = []
-
-      apps.each_with_index do |app, i|
-        if app[:available] == "1"
-          selected << app
-          Device::Display.print("#{app[:label]}", i+2)
-        end
-      end
-
-      selected[getc.to_i - 1]
-    end
+    getc
+    asdfasdk
   end
 end
 
 class CloudWalkInit
+  class << self
+    include Device::Helper
+  end
+
   def self.perform
-    set_logical_number
-    set_wifi_config
-    if params_dat
-      update_apps
-      execute(Main::Download.menu)
+    self.set_logical_number
+    self.set_wifi_config
+    if Device::ParamsDat.update_apps
+      self.execute(Device::ParamsDat.application_menu)
     end
   end
 
   def self.set_logical_number
     number = Device::Setting.logical_number
-    Device::Setting.logical_number = Main.form("Logical Number (#{number}): ", 0, 4, number, true)
+    Device::Setting.logical_number = form("Logical Number (#{number}): ", 0, 4, number, true)
   end
 
-  def self.set_wifi_config
+  def self.config
     Device::Display.clear
     puts "Would like to configure connection?"
     puts "1 - Yes"
@@ -167,63 +43,32 @@ class CloudWalkInit
     if (configure == "1")
       # TODO Scalone check media
       Device::Setting.media = Device::Network::MEDIA_WIFI
-      Device::Setting.authentication = Main.form(" Authentication (#{Device::Setting.authentication}): ", 0, 127, "", false)
-      Device::Setting.password       = Main.form(" Password (#{Device::Setting.password}): ", 0, 127, "", false)
-      Device::Setting.essid          = Main.form(" Essid (#{Device::Setting.essid}): ", 0, 127, "", false)
-      Device::Setting.channel        = Main.form(" Channel (#{Device::Setting.channel}): ", 0, 127, "", false)
-      Device::Setting.cipher         = Main.form(" Cipher (#{Device::Setting.cipher}): ", 0, 127, "", false)
-      Device::Setting.mode           = Main.form(" Mode (#{Device::Setting.mode}): ", 0, 127, "", false)
+      Device::Setting.authentication = form(" Authentication (#{Device::Setting.authentication}): ", 0, 127, "", false)
+      Device::Setting.password       = form(" Password (#{Device::Setting.password}): ", 0, 127, "", false)
+      Device::Setting.essid          = form(" Essid (#{Device::Setting.essid}): ", 0, 127, "", false)
+      Device::Setting.channel        = form(" Channel (#{Device::Setting.channel}): ", 0, 127, "", false)
+      Device::Setting.cipher         = form(" Cipher (#{Device::Setting.cipher}): ", 0, 127, "", false)
+      Device::Setting.mode           = form(" Mode (#{Device::Setting.mode}): ", 0, 127, "", false)
     end
   end
 
-  def self.set_gprs_config
-    # TODO Scalone We need  the same behaviour as numbers
-    Device::Setting.apn  = Main.form("Apn  (#{Device::Setting.apn}): ", 0, 127, "", false)
-    Device::Setting.user = Main.form("User (#{Device::Setting.user}): ", 0, 127, "", false)
-    Device::Setting.pass = Main.form("Pass (#{Device::Setting.pass}): ", 0, 127, "", false)
-  end
-
-  def self.params_dat
-    Device::Display.clear
-    Device::Display.print("Press to Download Params", 3)
-    getc
-    Main::Download.params_dat
-  end
-
-  def self.update_apps
-    Main::Download.update_apps
-  end
-
-  def self.execute(app)
-    return if app.nil? || ! File.exists?("./#{app[:mrb]}")
-    puts(require "./#{app[:mrb]}")
-    klass = Device::Support.path_to_class(app[:mrb])
-    klass.call if klass.respond_to? :call
-    getc
-  end
-end
-
-class DebugInit
-  def self.perform
-    config
-    execute(menu, :App)
-  end
-
-  def self.config
+  def self.set_wifi_config
     #WIFI
     Device::Setting.media          = Device::Network::MEDIA_WIFI
-    Device::Setting.authentication = Device::Network::AUTH_WPA2_PSK
-    Device::Setting.password       = "desgracapelada"
-    Device::Setting.essid          = "Barril do Chaves"
-    Device::Setting.channel        = "0"
-    Device::Setting.cipher         = Device::Network::PARE_CIPHERS_CCMP
     Device::Setting.mode           = Device::Network::MODE_STATION
 
+    #Device::Setting.authentication = Device::Network::AUTH_WPA2_PSK
+    #Device::Setting.cipher         = Device::Network::PARE_CIPHERS_CCMP
+    #Device::Setting.password       = "desgracapelada"
+    #Device::Setting.essid          = "Barril do Chaves"
+    #Device::Setting.channel        = "0"
+
     # WIFI Office
-    #Device::Setting.authentication = Device::Network::AUTH_WPA_WPA2_PSK
-    #Device::Setting.cipher         = Device::Network::PARE_CIPHERS_TKIP
-    #Device::Setting.password       = "planobesemfio"
-    #Device::Setting.essid          = "PlanoBe"
+    Device::Setting.authentication = Device::Network::AUTH_WPA_WPA2_PSK
+    Device::Setting.cipher         = Device::Network::PARE_CIPHERS_TKIP
+    Device::Setting.password       = "planobesemfio"
+    Device::Setting.essid          = "PlanoBe"
+    Device::Setting.channel        = "0"
 
     #GPRS
     #Device::Setting.mode           = Network::MEDIA_GPRS
@@ -233,44 +78,20 @@ class DebugInit
     #Device::Setting.pass           = "vivo"
   end
 
-  def self.menu
-    Device::Display.clear
-    Device::Display.print("Application Menu")
-    Device::Display.print("1 - Execute", 2)
-    Device::Display.print("2 - Update", 3)
-    Device::Display.print("3 - Load", 4)
-    Device::Display.print("4 - Remove", 5)
-    Device::Display.print("5 - Params", 6)
-    Device::Display.print("6 - TestWrite", 7)
-    Device::Display.print("7 - Download Apps", 8)
-    getc
+
+  def self.set_gprs_config
+    # TODO Scalone We need  the same behaviour as numbers
+    Device::Setting.apn  = form("Apn  (#{Device::Setting.apn}): ", 0, 127, "", false)
+    Device::Setting.user = form("User (#{Device::Setting.user}): ", 0, 127, "", false)
+    Device::Setting.pass = form("Pass (#{Device::Setting.pass}): ", 0, 127, "", false)
   end
 
-  def self.execute(option, app)
-    path     = "#{app.to_s.downcase}.mrb"
-    case option
-    when "1"
-      require(path)
-      Device::Support.path_to_class(path).call
-      getc
-    when "2"
-      Main::Download.update_app(path)
-    when "3"
-      puts "Load #{load(path)}"
-      getc
-    when "4"
-      puts "Delete #{File.delete(path)}"
-      getc
-    when "5"
-      Main::Download.params_dat
-      getc
-    when "6"
-      puts "form: #{Main.form("Logical Number: ", 0, 4, "1234", false)}"
-      getc
-    when "7"
-      Main::Download.update_apps
-      getc
-    end
+  # TODO Scalone: refactoring needed about app[:zip]
+  def self.execute(app)
+    Device::Display.clear
+    return if app.nil?
+    Miniz.unzip(app[:zip])
+    Device::Runtime.execute(app[:zip].split(".")[0])
   end
 end
 
