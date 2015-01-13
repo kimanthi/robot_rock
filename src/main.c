6be98f5bccef6926e6ec97cbf266f93aeb93f8c6
Thiago Scalone <thiago@scalone.com.br>
2015-01-13 20:41:10 -0200

    Remove pax_allocf function, not necessary for now.

--- src/main.c
+++ src/main.c
@@ -40,37 +40,6 @@
 #include "mruby/class.h"
 #include "mruby/debug.h"
 
-unsigned long total_allocated_mem = 0;
-int counter=0;
-
-void *pax_allocf(mrb_state *mrb, void *p, size_t size, void *ud)
-{
-  void *new_ptr;
-
-  if (size == 0) {
-    if (p) free(p);
-    return NULL;
-  }
-  else {
-    if (p)
-      new_ptr = realloc(p, size);
-    else
-      new_ptr = malloc(size);
-
-    if(!new_ptr){
-      /*printf("\nMemory Allocation Error: err %d total %lu object %d free %d", new_ptr, total_allocated_mem, size, freesize());*/
-      printf("\nMemory Allocation Error: err %d total %lu object %d", (int)new_ptr, total_allocated_mem, size);
-      /*kbflush();*/
-      /*getkey();*/
-    } else {
-      counter++;
-      total_allocated_mem += size;
-    }
-  }
-
-  return new_ptr;
-}
-
 mrb_state *mrb;
 
 mrb_state *mrb_robot(void)
