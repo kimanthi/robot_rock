83f8bee2601b361e56ecc392ade28c8eb69afa54
Thiago Scalone <thiago@scalone.com.br>
2016-12-27 15:08:34 -0200

    Remove ContextLog function, sent to mruby-context.

--- src/debugger.c
+++ src/debugger.c
@@ -13,8 +13,6 @@
 #include <unistd.h>
 #include "osal.h"
 
-#include "debugger.h"
-
 /**
  * call this function if you want to debug source code.
  * return 0 if successful
@@ -44,23 +42,5 @@ int start_debug()
 		ret = -1;
 	}
 	return ret;
-}
-
-void ContextLog(mrb_state *mrb, int severity_level, const char *format, ...)
-{
-  char dest[1024];
-  va_list argptr;
-
-  va_start(argptr, format);
-  vsprintf(dest, format, argptr);
-  va_end(argptr);
-
-  mrb_value msg, context;
-  context = mrb_const_get(mrb, mrb_obj_value(mrb->object_class), mrb_intern_lit(mrb, "ContextLog"));
-  msg = mrb_funcall(mrb, mrb_str_new(mrb, dest, strlen(dest)), "inspect", 0);
-  if (severity_level == 3) /*Error*/
-    mrb_funcall(mrb, context, "error", 1, msg);
-  else
-    mrb_funcall(mrb, context, "info", 1, msg);
 }
 
