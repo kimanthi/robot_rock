64188292d6d57aa9248528709ff2999e00a4b8db
Thiago Scalone <thiago@scalone.com.br>
2017-08-08 06:01:08 -0300

    Refactoring keyboard implementation to not close handle in a new function(main loop use).

--- src/keyboard.c
+++ src/keyboard.c
@@ -1,6 +1,13 @@
 #include "constant.h"
 #include "header.h"
 #include "keyboard.h"
+
+#include "mruby.h"
+#include "mruby/value.h"
+#include "mruby/compile.h"
+#include "mruby/proc.h"
+#include "../mrbgems/mruby-context/include/mruby/ext/context_log.h"
+
 #include <sys/time.h>
 
 /* Adds 'interval_ms' to timeval 'a' and store in 'result'
@@ -27,12 +34,9 @@ static int compare_timeval(struct timeval *a, struct timeval *b)
   return 0;
 }
 
-/**
- * keyboard test case
- * return KEY if successful
- * return 18 if timeout
- */
-int GetKey(long timeout)
+int fd = -1; //open(KEYBOARD_NAME, O_RDONLY | O_NONBLOCK);
+
+int GetMainKey(mrb_state *mrb, long timeout)
 {
   struct timeval tv1, tv2;
   int code = 18; // TIMEOUT
@@ -42,7 +46,10 @@ int GetKey(long timeout)
   int flag = 1;
   int event_num;
   int size = sizeof(struct input_event);
-  int fd = open(KEYBOARD_NAME, O_RDONLY | O_NONBLOCK);
+
+  if (fd < 0) {
+    fd = open(KEYBOARD_NAME, O_RDONLY | O_NONBLOCK);
+  }
 
   if (fd < 0) return code;
 
@@ -63,6 +70,66 @@ int GetKey(long timeout)
 
     event_num = rd / size;
     for (i = 0; i < event_num; ++i) {
+      /*ContextLog(mrb, 1, "type [%d] value [%d]", ev[i].type, ev[i].value);*/
+      /* value 1 means press key */
+      /* value 0 means release key */
+      /* value 2 means long press key */
+      if (ev[i].type == EV_KEY && (ev[i].value == 1 || ev[i].value == 2)){
+        code = ev[i].code;
+        flag = 0;
+      }
+    }
+  }
+
+  close(fd);
+  fd = open(KEYBOARD_NAME, O_RDONLY | O_NONBLOCK);
+  return code;
+}
+
+/**
+ * keyboard test case
+ * return KEY if successful
+ * return 18 if timeout
+ */
+int GetKey(mrb_state *mrb, long timeout)
+{
+  struct timeval tv1, tv2;
+  int code = 18; // TIMEOUT
+  int i;
+  int rd;
+  static struct input_event ev[64];
+  int flag = 1;
+  int event_num;
+  int size = sizeof(struct input_event);
+  /*int fd = open(KEYBOARD_NAME, O_RDONLY | O_NONBLOCK);*/
+
+  if (fd < 0) {
+    fd = open(KEYBOARD_NAME, O_RDONLY | O_NONBLOCK);
+  } else {
+    close(fd);
+    fd = open(KEYBOARD_NAME, O_RDONLY | O_NONBLOCK);
+  }
+
+  if (fd < 0) return code;
+
+  gettimeofday(&tv1, NULL);
+  add_ms_to_timeval(&tv1, timeout, &tv1);
+
+  while (flag == 1) {
+    rd = read(fd, ev, sizeof(ev));
+
+    if (rd < size) {
+      if (timeout != 0) {
+        gettimeofday(&tv2, NULL);
+        if (compare_timeval(&tv2, &tv1) == 1) break;
+      }
+
+      continue;
+    }
+
+    event_num = rd / size;
+    for (i = 0; i < event_num; ++i) {
+      /*ContextLog(mrb, 1, "type [%d] value [%d]", ev[i].type, ev[i].value);*/
       /* value 1 means press key */
       /* value 0 means release key */
       /* value 2 means long press key */
--- src/keyboard.h
+++ src/keyboard.h
@@ -1,11 +1,17 @@
 #ifndef KEYBOARD_H
 #define KEYBOARD_H
 
+#include "mruby.h"
+#include "mruby/value.h"
+#include "mruby/compile.h"
+#include "mruby/proc.h"
+
 /**
  * get key value (>=0), see macro in linux/input.h, such as KEY_1, KEY_CANCEL, etc.
  * return key value
  */
-int GetKey(long timeout);
+int GetKey(mrb_state *mrb, long timeout);
+int GetMainKey(mrb_state *mrb, long timeout);
 
 #endif
 
